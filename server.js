e727e7dccc577febecfc9b04ce41afc51296294e
Brandon McCraw <brandon.mccraw@provisionsgroup.com>
2022-07-16 10:07:44 -0500

    change port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const ronin = require('ronin-server')
 const mocks = require('ronin-mocks')
 
-const server = ronin.server()
+const server = ronin.server({
+  port:3000
+})
 
 server.use('/', mocks.server(server.Router(), false, true))
 server.start()
